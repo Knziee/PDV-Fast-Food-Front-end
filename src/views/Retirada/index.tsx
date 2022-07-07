ec9bff023b6ae2b968a67748c0d0f2b5a78cf257
Knziee <16rhuan16@gmail.com>
2022-07-07 18:27:53 -0300

    feat: page retirada created

--- src/views/Retirada/index.tsx
+++ src/views/Retirada/index.tsx
@@ -1,5 +1,26 @@
 import { Header } from "../../components/Header";
-
+import {
+  RetiradaBox,
+  RetiradaDivider,
+  RetiradaName,
+  RetiradaTitle,
+  RetiradaWrapper,
+} from "./styles";
 export const Retirada: React.FC = () => {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <RetiradaWrapper>
+        <RetiradaBox>
+          <RetiradaTitle>Preparando:</RetiradaTitle>
+          <RetiradaName>aaaa</RetiradaName>
+        </RetiradaBox>
+        <RetiradaDivider />
+        <RetiradaBox>
+          <RetiradaTitle>Pronto:</RetiradaTitle>
+          <RetiradaName nameReady>aaaa</RetiradaName>
+        </RetiradaBox>
+      </RetiradaWrapper>
+    </div>
+  );
 };
--- src/views/Retirada/styles.ts
+++ src/views/Retirada/styles.ts
@@ -1,5 +1,33 @@
 import styled from "styled-components";
 
-export const Teste = styled.div`
-  
+export const RetiradaWrapper = styled.div`
+  height: 100%;
+  max-width: 1440px;
+  border: 1px solid purple;
+  display: flex;
+  margin: auto;
+  flex-direction: row;
+  padding-top: 70px;
+  background-color: #f2eded;
+  display: flex;
+  justify-content: center;
+`;
+
+export const RetiradaDivider = styled.div`
+  height: 600px;
+  width: 5px;
+  background-color: black;
+`;
+
+export const RetiradaBox = styled.div`
+  width: 700px;
+`;
+export const RetiradaTitle = styled.div`
+  width: 700px;
+  font-size: 45px;
+`;
+export const RetiradaName = styled.div`
+  width: 700px;
+  font-size: 80px;
+  color: ${(props: any) => (props.nameReady ? "green" : "grey")};;
 `;
