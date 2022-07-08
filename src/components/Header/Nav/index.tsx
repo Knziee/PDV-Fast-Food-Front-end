9a55a9584ff616b23ff1357092e1aef26cd5702c
Knziee <16rhuan16@gmail.com>
2022-07-08 18:24:43 -0300

    fix: minor changes on navbar style

--- src/components/Header/Nav/index.tsx
+++ src/components/Header/Nav/index.tsx
@@ -1,29 +1,24 @@
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { NavContainer, NavItem } from "./styles";
 
 interface NavProps {}
 
 export const Nav: React.FC<NavProps> = () => {
-  const [onPedidos, setOnPedidos] = useState("");
-
   let navigate = useNavigate();
 
   const handleClick = () => {
     navigate("/");
-    setOnPedidos("pedidosProps");
   };
 
   return (
     <NavContainer>
-      <NavItem onPedidos onClick={handleClick} backgroundColor={onPedidos}>
+      <NavItem onPedidos onClick={handleClick}>
         Pedidos
       </NavItem>
       <NavItem
         onClick={() => {
           navigate("/cozinha");
         }}
-        backgroundColor={onPedidos}
       >
         Cozinha
       </NavItem>
@@ -31,9 +26,6 @@ export const Nav: React.FC<NavProps> = () => {
         onClick={() => {
           navigate("/retirada");
         }}
-        backgroundColor={onPedidos}
-        Link
-        to="/retirada"
       >
         Retirada
       </NavItem>
--- src/components/Header/Nav/styles.tsx
+++ src/components/Header/Nav/styles.tsx
@@ -2,15 +2,17 @@ import styled from "styled-components";
 
 export const NavContainer = styled.div`
   display: flex;
-  gap: 30px;
+  gap: 50px;
   color: white;
-  margin-right: 30px;
+  margin-right: 70px;
+  margin-top: 20px;
 `;
 
 export const NavItem = styled.div`
-  padding: 8px 20px;
-  border-radius: 10px;
-  background-color: ${(props: any) =>
-    props.pedidosProps ? "green" : props.cozinhaProps ? "green" : "green"};
+  border-bottom: 3px solid #125c13;
+  padding: 0px 0px 19px 0px;
+  :hover {
+    border-bottom: 3px solid #ffff;
+  }
+  cursor: pointer;
 `;
-//background-color: ${props => !props.isAvailable ? "#FBE192" : props.selected ? "#8DD7CF" : "#C3CFD9"};
